a53e8f706ba26f8d3ba7c3a535494221d46427db
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-30 23:06:00 +0100

    added stats route to the flask Blueprint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,20 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models.base_model import BaseModel
+from models import storage
 
-@app_views.route("/status")
+
+@app_views.route("/status", strict_slashes=False)
 def status():
     """return status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def obj_stats_view():
+    """retrieve the total number instan obj per each class"""
+    all_cls = BaseModel.__subclasses__()
+    cls_count = {}
+    for cls in all_cls:
+        cls_count[cls.__name__.lower()] = storage.count(cls)
+    return jsonify(cls_count)
