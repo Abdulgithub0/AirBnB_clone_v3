0345f7dd1a87e59ed87389efe02bd4b04e3085c6
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-10-02 20:29:55 +0100

    added RESTful api that handle the M-N between place objects and Amenity objects

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -32,7 +32,7 @@ def delete_a_place_amenity(place_id, amenity_id):
         abort(404)
     if storage_t == "db":
         for a in place.amenities:
-            if a["id"] == amenity_id:
+            if a.id == amenity_id:
                 check = 1
                 break
     elif not (amenity_id in places.amenity_ids):
@@ -50,15 +50,21 @@ def add_amenities_to_place(place_id, amenity_id):
     """add amenity to place object"""
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
+    check = None
     if not (place and amenity):
         abort(404)
     if storage_t == "db":
         for a in place.amenities:
-            if a["id"] == amenity_id:
+            if a.id == amenity_id:
                 check = 1
                 break
-    elif not (amenity_id in places.amenity_ids):
+    elif (amenity_id in places.amenity_ids):
         check = 1
     if check:
         return jsonify(amenity.to_dict())
-    return jsonify(amenity), 201
+    if storage_t == "db":
+        place.amenities.append(amenity)
+    else:
+        place.amenity_ids.append(amenity_id)
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
